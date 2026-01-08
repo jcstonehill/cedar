09b27cace7ce330c6e2b6558c219d3a7f15b2576
Jacob Stonehill <jakestonehill@gmail.com>
2026-01-08 00:28:24 -0500

    Try to fix issue with docs build.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,24 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import sys, os
-
-# Determine if we're on Read the Docs server
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-
-# On Read the Docs, we need to mock a few third-party modules so we don't get
-# ImportErrors when building documentation
-from unittest.mock import MagicMock
-
-MOCK_MODULES = [
-    'openmoc', 'openmc.data.reconstruct',
-]
-sys.modules.update((mod_name, MagicMock()) for mod_name in MOCK_MODULES)
-
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+import sys, os
 sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
