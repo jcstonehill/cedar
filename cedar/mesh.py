Fixed to harmonic average of k

cedar/mesh.py
@@ -35,11 +35,13 @@ class Mesh(ABC):
         self.face_cells_i: np.ndarray = []
         self.face_is_interior: np.ndarray = []
 
-        self.cell_centers: np.ndarray = []
-        self.face_centers: np.ndarray = []
         self.face_n: np.ndarray = []
         self.face_d: np.ndarray = []
+        self.face_w: np.ndarray = []
 
+        self.cell_centers: np.ndarray = []
+        self.face_centers: np.ndarray = []
+        
         self.region_i: dict[str, np.ndarray] = {}
         self.region_N: dict[str, int] = {}
 
@@ -116,11 +118,13 @@ class Mesh1D(Mesh):
         self.face_cells_i: np.ndarray = []
         self.face_is_interior: np.ndarray = []
 
-        self.cell_centers: np.ndarray = []
-        self.face_centers: np.ndarray = []
         self.face_n: np.ndarray = []
         self.face_d: np.ndarray = []
+        self.face_w: np.ndarray = []
 
+        self.cell_centers: np.ndarray = []
+        self.face_centers: np.ndarray = []
+        
         self.region_i: dict[str, np.ndarray] = {}
         self.region_N: dict[str, int] = {}
 
@@ -154,6 +158,14 @@ class Mesh1D(Mesh):
         for i in range(self.N_cells):
             self.cell_centers[i][:] = self.start + delta*self.ds*(0.5+i)
         self.face_centers: np.ndarray = np.copy(self.pts) # For 1D, points and faces are the same thing
+
+        self.face_n: np.ndarray = np.full((self.N_faces, 3), self.basis_vec[self.basis], dtype = np.float64)
+        self.face_d: np.ndarray = np.full((self.N_faces, 2), self.ds/2, dtype = np.float64)
+        self.face_d[0][1] = 0
+        self.face_d[-1][1] = 0
+        self.face_w: np.ndarray = np.full(self.N_faces, 0.5)
+        self.face_w[0] = 1
+        self.face_w[-1] = 1
 
         self.region_i: dict[str, np.ndarray] = {self.regions[0] : np.arange(self.N_cells)}
         self.region_N: dict[str, int] = {self.regions[0] : self.N_cells}
@@ -224,6 +236,10 @@ class Mesh3D(Mesh):
         self.face_cells_i: np.ndarray = []
         self.face_is_interior: np.ndarray = []
 
+        self.face_n: np.ndarray = []
+        self.face_d: np.ndarray = []
+        self.face_w: np.ndarray = []
+
         self.cell_centers: np.ndarray = []
         self.face_centers: np.ndarray = []
 
@@ -236,8 +252,6 @@ class Mesh3D(Mesh):
         # Specific to 3D
         self.cell_vols: np.ndarray = []
         self.face_areas: np.ndarray = []
-        self.face_n: np.ndarray = []
-        self.face_d: np.ndarray = []
         self.region_vol: dict[str, float] = {}
         self.boundary_area: dict[str, float] = {}
 
@@ -361,6 +375,8 @@ class Mesh3D(Mesh):
         d2[~self.face_is_interior] = 0
 
         self.face_d = np.stack((d1, d2), axis=1).astype(np.float64)
+
+        self.face_w = 1 - d1/(d1+d2)
 
         for boundary in self.boundaries:
             self.boundary_area[boundary] = np.sum(self.face_areas[self.boundary_i[boundary]])

cedar/models/heat_transfer.py
@@ -496,15 +496,14 @@ class HeatTransfer(cedar.Model):
         :param k_cell: Cell thermal conductivity.
         :return: Face thermal conductivity.
         """
-        k_face = np.empty(self.mesh.N_faces)
+        k_face = np.empty(self.mesh.N_faces, dtype = np.float64)
 
         f = self.mesh.face_is_interior
         c1 = self.mesh.face_cells_i[f, 0]
         c2 = self.mesh.face_cells_i[f, 1]
-        d1 = self.mesh.face_d[f, 0]
-        d2 = self.mesh.face_d[f, 1]
+        w = self.mesh.face_w[f]
 
-        k_face[f] = k_cell[c1]*k_cell[c2] / ((k_cell[c1]*d2 + k_cell[c2]*d1)/(d1+d2))
+        k_face[f] = self._k_face_interior(k_cell[c1], k_cell[c2], w)
 
         # boundary faces
         bf = ~f
@@ -591,3 +590,6 @@ class HeatTransfer(cedar.Model):
         A = coo_matrix((data, (rows, cols)), shape=(N, N)).tocsr()
 
         return A, b
+
+    def _k_face_interior(self, k1, k2, w):
+        return 1/((1-w)/k1 + w/k2)